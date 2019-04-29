b26ebc138eee2ed6f2142d6af8def7656365f031
Jose Antonio Rapozo <jose.rezende@mv.com.br>
2019-04-29 17:47:22 -0300

    Adicionando invert

--- src/rendering/renderWebImage.js
+++ src/rendering/renderWebImage.js
@@ -1,5 +1,6 @@
 import setToPixelCoordinateSystem from '../setToPixelCoordinateSystem.js';
 import { renderColorImage } from './renderColorImage.js';
+import { invertImage } from './invertImage';
 import saveLastRendered from './saveLastRendered.js';
 /**
  * API function to draw a standard web image (PNG, JPG) to an enabledImage
@@ -9,7 +10,7 @@ import saveLastRendered from './saveLastRendered.js';
  * @returns {void}
  * @memberof rendering
  */
-export function renderWebImage (enabledElement, invalidated) {
+export function renderWebImage(enabledElement, invalidated) {
   if (enabledElement === undefined) {
     throw new Error('renderWebImage: enabledElement parameter must not be undefined');
   }
@@ -26,30 +27,48 @@ export function renderWebImage (enabledElement, invalidated) {
         enabledElement.viewport.voi.windowCenter === enabledElement.image.windowCenter &&
         enabledElement.viewport.invert === false) {*/
 
-    // Get the canvas context and reset the transform
-    const context = enabledElement.canvas.getContext('2d');
+  // Get the canvas context and reset the transform
+  const context = enabledElement.canvas.getContext('2d');
 
-    context.setTransform(1, 0, 0, 1, 0, 0);
+  context.setTransform(1, 0, 0, 1, 0, 0);
 
-    // Clear the canvas
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
+  // Clear the canvas
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
 
-    // Turn off image smooth/interpolation if pixelReplication is set in the viewport
-    context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
-    context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
+  // Turn off image smooth/interpolation if pixelReplication is set in the viewport
+  context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
+  context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
 
-    // Save the canvas context state and apply the viewport properties
-    setToPixelCoordinateSystem(enabledElement, context);
+  // Save the canvas context state and apply the viewport properties
+  setToPixelCoordinateSystem(enabledElement, context);
 
-    const sx = enabledElement.viewport.displayedArea.tlhc.x - 1;
-    const sy = enabledElement.viewport.displayedArea.tlhc.y - 1;
-    const width = enabledElement.viewport.displayedArea.brhc.x - sx;
-    const height = enabledElement.viewport.displayedArea.brhc.y - sy;
+  const sx = enabledElement.viewport.displayedArea.tlhc.x - 1;
+  const sy = enabledElement.viewport.displayedArea.tlhc.y - 1;
+  const width = enabledElement.viewport.displayedArea.brhc.x - sx;
+  const height = enabledElement.viewport.displayedArea.brhc.y - sy;
 
-    context.drawImage(image.getImage(), sx, sy, width, height, 0, 0, width, height);
+  var image = image.getImage();
 
-    enabledElement.renderingTools = saveLastRendered(enabledElement);
+  if (enabledElement.viewport.invert) {
+    image
+  }
+
+  context.drawImage(image, sx, sy, width, height, 0, 0, width, height);
+
+  var imgData = context.getImageData(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
+  var data = imgData.data;
+
+  for (var i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i];
+    data[i + 1] = 255 - data[i + 1];
+    data[i + 2] = 255 - data[i + 2];
+  }
+
+  context.putImageData(imgData, 0, 0);
+
+  enabledElement.renderingTools = saveLastRendered(enabledElement);
+
   /*} else {
     renderColorImage(enabledElement, invalidated);
   }*/
